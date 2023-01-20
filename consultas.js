13b6a84933a55011969513d395cb5ba53ac122cd
Dgutier08 <102337196+Dgutier08@users.noreply.github.com>
2023-01-19 21:40:43 -0300

    se agrega middleware

--- consultas.js
+++ consultas.js
@@ -10,60 +10,53 @@ const pool = new Pool({
 });
 
 //aplicando Offset
-const getJoyas = async ({ limits = 5, order_by = "id_ASC", page = 1 }) => {
+const getJoyas = async ({ limits = 10, order_by = "id_ASC", page = 1 }) => {
     const [campo, direccion] = order_by.split("_")
     const offset = (page - 1) * limits;
-    const formattedQuery = format(
-        "SELECT * FROM joyas order by %s %s limit %s OFFSET %S",
-        campo,
-        direccion,
-        limits,
-        offset
-    )
-      const { rows: joyas, rowCount } = await pool.query(formattedQuery)
+    const formattedQuery = format('SELECT * FROM inventario order by %s %s LIMIT %s OFFSET %s', campo, direccion, limits, offset);
+    const { rows: articulo, rowCount } = await pool.query(formattedQuery)
     
     if (rowCount === 0) {
         throw { code: 404, message: `No se encontraron resultados` };
       };
     
-    return joyas;
+    return articulo;
     }
 
     //con filtros
-    const getJoyasFiltro = async ({ precio_max, stock_min }) => {
-        let filtros = [];
-        const values = [];
+    const getJoyasFiltro = async ({ precio_max, precio_min, categoria, metal }) => {
+        let filtros = []
+        const values = []
         const agregarFiltro = (campo, comparador, valor) => {
-            values.push(valor);
-            const { length } = filtros;
-
-            filtros.push(`${campo} ${comparador} $${length + 1}`);
-        };
-        if (precio_max) agregarFiltro("precio", "<=", precio_max);
-        if (stock_min) agregarFiltro("stock", ">=", stock_min);
-        if (categoria) agregaFiltro ("categoria", "=", categoria)
-        if (metal) agregaFiltro ("metal", "=", metal)
-
-
-        let consulta = "SELECT * FROM inventario";
-
-        if (filtros.length > 0) {
-            filtros = filtros.join(" AND ");
-            consulta += ` WHERE ${filtros}`;
+            values.push(valor)
+            const { length } = filtros
+            filtros.push(`${campo} ${comparador} $${length + 1}`)
         }
-        const { rows: Joyas } = await pool.query(consulta, values);
-        return Joyas;
-    };
+        if (precio_max) agregarFiltro('precio', '<=', precio_max)
+        if (precio_min) agregarFiltro('precio', '>=', precio_min)
+        if (categoria) agregarFiltro('categoria', '=', categoria)
+        if (metal) agregarFiltro('metal', '=', metal)
+    
+    
+        let consulta = "SELECT * FROM inventario"
+        if (filtros.length > 0) {
+            filtros = filtros.join(" AND ")
+            consulta += ` WHERE ${filtros}`
+        }
+        const { rows: inventario } = await pool.query(consulta, values)
+        return inventario
+    }
+    
 
     const datosHATEOAS = (joyas) => {
-        const results = joyas.map((j) => {
+        const results = joyas.map((x) => {
           return {
-            name: j.nombre,
-            href: `joyas/joya/${j.id}`,
+            name: x.nombre,
+            href: `joyas/joya/${x.id}`,
           }
         });
         const totalJoyas = joyas.length
-        const totalStock =  joyas.reduce((total, j) => total + j.stock, 0);
+        const totalStock =  joyas.reduce((total, x) => total + x.stock, 0);
         const HATEOAS = {
           totalJoyas,
           totalStock,
--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 const express = require("express");
+const { reportRequest} = require("./logger") 
 const app = express();
 
 const {getJoyas, getJoyasFiltro, datosHATEOAS } = require("./consultas");
 
 
-app.get("/joyas", async (req, res) => {
-    try{
-        const queryStrings = req.query;
-        const joyas = await getJoyas(queryStrings);
-        const HATEOAS= await datosHATEOAS(joyas);
-        res.status(200).json(HATEOAS);
-    } catch (error){
-        console.error(error);
+app.get('/joyas', reportRequest, async (req, res) => {
+    try {
+      const queryStrings = req.query;
+      const inv = await getJoyas(queryStrings);
+      const HATEOAS = await datosHATEOAS(inv);
+      res.json(HATEOAS);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error.message);
     }
+  })
   
-  });
-  
-  app.get("/joyas/filtros", async (req, res) => {
-    const queryStrings = req.query;
-    const joyas = await getJoyasFiltro(queryStrings);
-    res.status(200).json(joyas);
+  app.get("/joyas/filtros", reportRequest, async (req, res) => {
     try{
+    const queryStrings = req.query;
+    const catalogo = await getJoyasFiltro(queryStrings);
+    res.status(200).json(catalogo);
+    
     } catch (error){
         console.error(error);
+        res.status(500).send(error.message);
     }
 });
   
-  app.get("*", (req, res) => {
-    res.status(404).json({ message: "Esta ruta no existe" });
-  });
+   app.get("*", (req, res) => {
+     res.status(404).json({ message: "Esta ruta no existe" });
+   });
   
   app.listen(3000, console.log("Escuchando por el puerto 3000"));
